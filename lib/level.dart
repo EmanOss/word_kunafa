16ad7f47848a993747b89fa41d6ffa2d1fb2b8b3
Iman <iman.osama2000@gmail.com>
2022-08-01 22:01:22 +0200

    correct and bonus columns

---
+++ lib/level.dart
@@ -0,0 +1,15 @@
+class level {
+  //data Type
+  List<dynamic>? letters;
+  List<dynamic>? correct;
+  List<dynamic>? bonus;
+// constructor
+  level({this.letters, this.correct, this.bonus});
+
+  //method that assign values to respective datatype vairables
+  level.fromJson(Map<String, dynamic> json) {
+    letters = List.from(json['letters']);
+    correct = List.from(json['correct']);
+    bonus = List.from(json['bonus']);
+  }
+}
--- lib/screens/play.dart
+++ lib/screens/play.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
+import '../level.dart';
 import 'my_home.dart';
 import 'package:flutter/services.dart' as rootBundle;
 
@@ -16,55 +17,75 @@ class _myStyles {
   static final title = TextStyle(
     fontSize: 25,
   );
-}
-
-class level {
-  //data Type
-  // int? number;
-  List<String>? letters;
-  List<String>? correct;
-  List<String>? bonus;
-  // String? letters;
-  // String? correct;
-  // String? bonus;
-
-// constructor
-  level({this.letters, this.correct, this.bonus});
-
-  //method that assign values to respective datatype vairables
-  level.fromJson(Map<String, dynamic> json) {
-    // number = json['id'];
-    letters = json['letters'];
-    correct = json['correct'];
-    bonus = json['bonus'];
-  }
+  static final words = TextStyle(
+    fontSize: 25,
+  );
 }
 
 class _PlayScreenState extends State<PlayScreen> {
   String _word = "";
-
-  // List<String> _correct = [];
+  List<String> _solved = [];
+  List<String> _solvedBonus = [];
+  int _currLevel=0;
+  // List<String> currCorrect=[];
+  // List<String> currBonus=[];
+  // List<String> currLetters=[];
+  List<level> allLevels=[];
 
   void _addLetter(String c) {
     setState(() {
       _word += c;
     });
   }
+  void _clearWord() {
+    setState(() {
+      _word ="";
+    });
+  }
+  int _checkWord(String w){
+    //retuns 1 if word is correct, 2 if it's in the bonus list, 0 if it's wrong
+    return(allLevels[_currLevel].correct!.contains(w)&& !_solved.contains(w))?
+     1: (allLevels[_currLevel].bonus!.contains(w) && !_solvedBonus.contains(w))? 2:0;
+  }
+  void _addSolvedWord(String w) {
+    int check = _checkWord(w);
+    if(check==1) {
+      setState(() {
+        _solved.add(w);
+      });
+      _clearWord();
+    }
+    else
+      if(check==2) {
+        setState(() {
+          _solvedBonus.add(w);
+        });
+        _clearWord();
+      }
+    else {
+      //todo add err msg/indication
+        _clearWord();
+      }
+  }
 
   void _back() {
     Navigator.pop(context);
   }
 
+
   Future<List<level>> ReadJsonData() async {
     //read json file
-    final jsondata =
-        await rootBundle.rootBundle.loadString('data_repo/levels.json');
+    final jsondata = await rootBundle.rootBundle.loadString('data_repo/levels.json');
     //decode json data as list
     final list = json.decode(jsondata) as List<dynamic>;
-
     //map json and initialize using DataModel
-    return list.map((e) => level.fromJson(e)).toList();
+    var res = list.map((e) => level.fromJson(e)).toList();
+    setState(() {
+      allLevels = res;
+    });
+    return res;
   }
+
 
   @override
   Widget build(BuildContext context) {
@@ -74,9 +95,9 @@ class _PlayScreenState extends State<PlayScreen> {
         // the App.build method, and use it to set our appbar title.
         title: Center(
             child: Text(
-          "مستوى 1",
-          style: _myStyles.title,
-        )),
+              (_currLevel+1).toString()+" مستوى ",
+              style: _myStyles.title,
+            )),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
@@ -99,8 +120,22 @@ class _PlayScreenState extends State<PlayScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Card(
-                child: FutureBuilder(
-              future: ReadJsonData(),
+                child:
+        Card(child:
+        Padding(padding: const EdgeInsets.all(10),
+        child: Row(children: [
+          // Row(children: [Text("solved"),const Spacer(flex:1), Text("Bonus")],),
+          Column(children: _solved.map<Card>((s) =>Card(child:Center(child:Text(s, style: _myStyles.words,),))).toList(),),
+          const Spacer(flex: 1,),
+          Column(children: _solvedBonus.map<Card>((s) =>Card(child:Center(child:Text(s, style: _myStyles.words,),))).toList(),),
+        ],)))
+            ),
+            Text(
+              _word,
+              style: _myStyles.letters,
+            ),
+            FutureBuilder(
+            future: ReadJsonData(),
               builder: (context, data) {
                 if (data.hasError) {
                   //in case if error found
@@ -110,157 +145,50 @@ class _PlayScreenState extends State<PlayScreen> {
                   // items will hold all the data of DataModel
                   //items[index].name can be used to fetch name of product as done below
                   var items = data.data as List<level>;
-                  return Expanded(
-                    child: SizedBox(
-                      height: 200.0,
-                      child: ListView.builder(
-                          itemCount: items == null ? 0 : items.length,
-                          itemBuilder: (context, index) {
-                            return Card(
-                              elevation: 5,
-                              margin: EdgeInsets.symmetric(
-                                  horizontal: 10, vertical: 6),
-                              child: Container(
-                                padding: EdgeInsets.all(8),
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  children: [
-                                    Expanded(
-                                        child: Container(
-                                      padding: EdgeInsets.only(bottom: 8),
-                                      child: Column(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          Padding(
-                                            padding: EdgeInsets.only(
-                                                left: 8, right: 8),
-                                            child: ListView.builder(
-                                                padding: const EdgeInsets.all(8),
-                                                itemCount: items[index].letters?.length,
-                                                itemBuilder: (BuildContext context, int i) {
-                                                  return Container(
-                                                    child: Center(child: Text('Entry ${items[index].letters![i]}')),
-                                                  );
-                                                }
-                                            )
-                                            // Text(
-                                            //   items[index].letters.toString(),
-                                            //   style: const TextStyle(
-                                            //       fontSize: 16,
-                                            //       fontWeight: FontWeight.bold),
-                                            // ),
-                                          ),
-                                          // Padding(
-                                          //   padding: EdgeInsets.only(
-                                          //       left: 8, right: 8),
-                                          //   child: Text(
-                                          //     items[index].correct.toString(),
-                                          //     style: const TextStyle(
-                                          //         fontSize: 16,
-                                          //         fontWeight: FontWeight.bold),
-                                          //   ),
-                                          // ),
-                                          // Padding(
-                                          //   padding: EdgeInsets.only(
-                                          //       left: 8, right: 8),
-                                          //   child: Text(
-                                          //     items[index].bonus.toString(),
-                                          //     style: const TextStyle(
-                                          //         fontSize: 16,
-                                          //         fontWeight: FontWeight.bold),
-                                          //   ),
-                                          // ),
-                                        ],
-                                      ),
-                                    ))
-                                  ],
-                                ),
+
+                  return Flex(direction: Axis.horizontal,
+                    children: [ Expanded(
+                      child: SizedBox(
+                        height: 200.0,
+                        child: ListView.builder(
+                          // shrinkWrap: false,
+                            itemCount: items[_currLevel].letters == null ? 0 : items[_currLevel].letters!.length,
+                            itemBuilder: (context, index) {
+                              // print("lenght hena");
+                              // print(items[_currLevel].letters!.length);
+                              return  Container(child:
+                              Row(children: [
+                                const Spacer(flex: 1,),
+                                Column(
+                                children: [
+                                  GestureDetector(
+                                    onTap: () => _addLetter(items[_currLevel].letters![index]),
+                                    child: Text(items[_currLevel].letters![index], style: _myStyles.letters),
+                                  ),
+                                ],
                               ),
-                            );
-                          }),
-                    ),
-                  );
-                } else {
+                                const Spacer(flex: 1,),
+                                // const Spacer(flex: 1,),
+                              ],),);
+                            }),
+                      ),
+                    )
+                    ],);
+                }
+                else {
                   // show circular progress while data is getting fetched from json file
                   return const Center(
                     child: CircularProgressIndicator(),
                   );
                 }
               },
-            )
-                // Text(_correct, style: _myStyles.title,),
-                ),
-            Text(
-              _word,
-              style: _myStyles.letters,
             ),
-            Row(
-              children: [
-                const Spacer(flex: 1),
-                // Column(
-                //   children: [],
-                // ),
-                Column(
-                  children: [
-                    GestureDetector(
-                      onTap: () => _addLetter("ر"),
-                      child: Text(
-                        "ر",
-                        style: _myStyles.letters,
-                      ),
-                    ),
-                  ],
-                ),
-                // Column(
-                //   children: [],
-                // ),
-                const Spacer(flex: 1),
-              ],
-            ),
-            Row(
-              children: [
-                const Spacer(flex: 1),
-                Column(
-                  children: [
-                    GestureDetector(
-                      onTap: () => _addLetter("ت"),
-                      child: Text("ت", style: _myStyles.letters),
-                    ),
-                  ],
-                ),
-                // Column(
-                //   children: [Text("")],
-                // ),
-                const Spacer(flex: 1),
-                Column(
-                  children: [
-                    GestureDetector(
-                      onTap: () => _addLetter("م"),
-                      child: Text("م", style: _myStyles.letters),
-                    ),
-                  ],
-                ),
-                const Spacer(flex: 1),
-              ],
-            ),
-            // const Spacer(flex: 1),
             ElevatedButton(
-                onPressed: () => setState(() {
-                      _word = "";
-                    }),
+                onPressed: () => _clearWord(),
                 child: const Icon(Icons.refresh)),
             ElevatedButton(
-                onPressed: () => setState(() {
-                      // if()
-                      // _correct.add(_word);
-                      _word = "";
-                    }),
+                onPressed: () => _addSolvedWord(_word),
                 child: const Icon(Icons.check)),
-            // Text("مسح", style: _myStyles.title)),
           ],
         ),
       ),
